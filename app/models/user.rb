5e4f972fa353d7fd13319db355d420aabeaf2103
hibi73 <66819398+hibi73@users.noreply.github.com>
2020-08-18 16:37:38 +0900

    インデントを整えた

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-  validates :nickname
-  zenkaku = /\A[ぁ-んァ-ン一-龥]/
-  validates :last_name, format: { with: zenkaku, message: "は全角で入力してください。"}
-  validates :first_name, format: { with: zenkaku, message: "は全角で入力してください。"}
-  zenkaku_kana = /\A[ァ-ヶー－]+\z/
-  validates :last_name_kana, format: { with: zenkaku_kana, message: "は全角カタカナで入力して下さい。"}
-  validates :first_name_kana, format: { with: zenkaku_kana, message: "は全角カタカナで入力して下さい。"}
-  validates :birthday
-  end
+    validates :nickname
+    zenkaku = /\A[ぁ-んァ-ン一-龥]/
+    validates :last_name, format: { with: zenkaku, message: "は全角で入力してください。"}
+    validates :first_name, format: { with: zenkaku, message: "は全角で入力してください。"}
+    zenkaku_kana = /\A[ァ-ヶー－]+\z/
+    validates :last_name_kana, format: { with: zenkaku_kana, message: "は全角カタカナで入力して下さい。"}
+    validates :first_name_kana, format: { with: zenkaku_kana, message: "は全角カタカナで入力して下さい。"}
+    validates :birthday
+    end
   validate :password_complexity
-
 end
 
 def password_complexity
