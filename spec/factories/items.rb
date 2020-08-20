b87c0807428dc6c012864aaf76c79b61792bee8b
hibi73 <66819398+hibi73@users.noreply.github.com>
2020-08-20 19:21:01 +0900

    factory_botの準備

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    image {Faker::Lorem.sentence}
+    name {Faker::Lorem.sentence}
+    description {Faker::Lorem.sentence}
+    category {Faker::Number.within(range: 2..11)}
+    status {Faker::Number.within(range: 2..7)}
+    burden {Faker::Number.within(range: 2..3)}
+    region {Faker::Number.within(range: 2..48)}
+    days {Faker::Number.within(range: 2..4)}
+    price {Faker::Number.within(range: 300..9999999)}
+    association :user
   end
 end
