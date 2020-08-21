856236e0715f1d2f75dedec22464ec1e1fc0739c
hibi73 <66819398+hibi73@users.noreply.github.com>
2020-08-21 18:21:51 +0900

    テストコード記入

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,17 +11,102 @@ RSpec.describe Item, type: :model do
       expect(@item).to be_valid
     end
 
-    it 'が空では保存できないこと' do
-      
+    it 'imageが空では保存できないこと' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
     end
 
-    it 'が空では保存できないこと' do
-      
+    it 'nameが空では保存できないこと' do
+      @item.name = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
     end
 
-    it 'が空では保存できないこと' do
-     
+    it 'descriptionが空では保存できないこと' do
+      @item.description = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Description can't be blank")
     end
+
+    it 'category_idが空では保存できないこと' do
+      @item.category_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank")
+    end
+
+    it 'category_idが1のとき保存できないこと' do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category must be other than 1")
+    end
+
+    it 'status_idが空では保存できないこと' do
+      @item.status_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Status can't be blank")
+    end
+
+    it 'status_idが1の時では保存できないこと' do
+      @item.status_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Status must be other than 1")
+    end
+
+    it 'burden_idが空では保存できないこと' do
+      @item.burden_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Burden can't be blank")
+    end
+
+    it 'burden_idが1の時では保存できないこと' do
+      @item.burden_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Burden must be other than 1")
+    end
+
+    it 'region_idが空では保存できないこと' do
+      @item.region_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Region can't be blank")
+    end
+
+    it 'region_idが1の時では保存できないこと' do
+      @item.region_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Region must be other than 1")
+    end
+
+    it 'days_idが空では保存できないこと' do
+      @item.days_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Days can't be blank")
+    end
+
+    it 'days_idが1の時では保存できないこと' do
+      @item.days_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Days must be other than 1")
+    end
+
+    it 'priceが空では保存できないこと' do
+      @item.price = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it 'priceが300より小さいと保存できないこと' do
+      @item.price = 299
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be greater than 300")
+    end
+
+    it 'priceが9999999より大きいと保存できないこと' do
+      @item.price = 10000000
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be less than 9999999")
+    end
+
   end
   
 end
