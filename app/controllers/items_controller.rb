679090c474c001dddb87822d2935c4dc0235b49a
hibi73 <66819398+hibi73@users.noreply.github.com>
2020-08-21 14:11:44 +0900

    カラム名変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,7 +15,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:image, :name, :description, :category, :status, :burden, :region, :days, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :burden_id, :region_id, :days_id, :price).merge(user_id: current_user.id)
   end
 
   def move_to_new_user_session
