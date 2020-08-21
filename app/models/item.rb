11192c4c118c89635dbeed538394e3015a8053cc
hibi73 <66819398+hibi73@users.noreply.github.com>
2020-08-21 12:04:34 +0900

    カラム名変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,11 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :description
-    validates :category
-    validates :status
-    validates :burden
-    validates :region
-    validates :days
+    validates :category_id, numericality: { other_than: 1 } 
+    validates :status_id, numericality: { other_than: 1 } 
+    validates :burden_id, numericality: { other_than: 1 } 
+    validates :region_id, numericality: { other_than: 1 } 
+    validates :days_id, numericality: { other_than: 1 } 
     validates :price, numericality: { only_integer: true,greater_than: 300, less_than: 9999999}
   end
 
