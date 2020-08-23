deec1032a98ca22beb507735689fc17e61334fe0
hibi73 <66819398+hibi73@users.noreply.github.com>
2020-08-23 18:16:42 +0900

    出品者以外はルートパスへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_new_user_session, except: [:index, :show]
+  before_action :move_to_root_path, except: [:index, :new, :create, :show]
 
   def index
     @items = Item.all
@@ -43,6 +44,13 @@ class ItemsController < ApplicationController
   def move_to_new_user_session
     unless user_signed_in?
       redirect_to new_user_session_path
+    end
+  end
+
+  def move_to_root_path
+    item =Item.find(params[:id])
+    if user_signed_in? && current_user.id != item.user.id
+      redirect_to root_path
     end
   end
 
