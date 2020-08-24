534d4b1e41cb1826dfc234d603426e7c10fd5790
hibi73 <66819398+hibi73@users.noreply.github.com>
2020-08-24 19:16:29 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "items#index"
-  resources :items, only: [:new, :create, :show, :edit, :update, :destroy]
-  resources :purchases, only:[:index, :create]
+  resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :purchases, only:[:index, :create]
+  end
 end
