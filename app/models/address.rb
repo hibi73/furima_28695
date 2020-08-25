e9033b4149d4cdb96c38686eafbeee0b1088961b
hibi73 <66819398+hibi73@users.noreply.github.com>
2020-08-25 11:56:19 +0900

    バリデーション設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,11 @@
 class Address < ApplicationRecord
   belongs_to :purchase
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefectures, numericality: { other_than: 1 }
+    validates :municipality
+    validates :house_number
+    validates :phone_number, format: {with: /^(0{1}\d{9,10})$/}
+  end
 end
