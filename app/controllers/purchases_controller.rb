080c12cee5559244ccba253b423aff3f520b226b
hibi73 <66819398+hibi73@users.noreply.github.com>
2020-08-25 12:34:38 +0900

    コントローラの設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,6 +6,28 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @purchase = Purchase.new
+    if @purchase.valid?
+      pay_item
+      @purchase.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.permit(:token)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      card: order_params[:token],    
+      currency:'jpy'                 
+    )
   end
   
 end
