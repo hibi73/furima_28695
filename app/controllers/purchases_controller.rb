d81c8359397aeb92485d2770dd9e2cba42a70fae
hibi73 <66819398+hibi73@users.noreply.github.com>
2020-08-27 16:55:38 +0900

    購入ページへの制限

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,6 @@
 class PurchasesController < ApplicationController
+  before_action :move_to_new_user_session
+  before_action :move_to_root_path,
 
   def index
     @purchase = PurchaseAddress.new
@@ -18,6 +20,19 @@ class PurchasesController < ApplicationController
   end
 
   private
+
+  def move_to_new_user_session
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
+  def move_to_root_path
+    item =Item.find(params[:item_id])
+    if user_signed_in? && current_user.id == item.user.id
+      redirect_to root_path
+    end
+  end
 
   def order_params
     params.require(:purchase_address).permit(:token, :postal_code, :prefectures, :municipality, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
