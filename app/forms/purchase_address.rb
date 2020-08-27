754c0c97a6298a76a76b64f580adc2167a4a981f
hibi73 <66819398+hibi73@users.noreply.github.com>
2020-08-27 21:17:44 +0900

    トークンのバリデーションを追加

--- app/forms/purchase_address.rb
+++ app/forms/purchase_address.rb
@@ -4,6 +4,7 @@ class PurchaseAddress
   attr_accessor :token, :postal_code, :prefectures, :municipality, :house_number, :building_name, :phone_number, :user_id, :item_id
 
   with_options presence: true do
+    validates :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefectures, numericality: { other_than: 1 }
     validates :municipality
