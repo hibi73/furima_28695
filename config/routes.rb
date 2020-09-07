5926797b471e2e88e45f30a4b003d91114c23148
hibi73 <66819398+hibi73@users.noreply.github.com>
2020-09-07 11:26:16 +0900

    searchアクションのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :purchases, only:[:index, :create]
     resources :comments, only: :create
+    collection do
+      get 'search'
+    end
   end
 end
